8ef24e2d13cc03d502c34765b8c6eb4f278494c5
loveblue0121 <loveblue0121@gmail.com>
2021-07-16 00:34:13 +0800

    Google MAP API 套用

--- src/pages/Course/CourseList/CourseMapModal.js
+++ src/pages/Course/CourseList/CourseMapModal.js
@@ -1,9 +1,46 @@
 import React, { useState } from 'react';
 import { FiSearch, FiMapPin, FiX } from 'react-icons/fi';
-//import Modal from 'react-modal';
+import GoogleMapReact from 'google-map-react';
+
+const MarkerIcon = () => {
+  return (
+    <>
+      <FiMapPin className="modal__marker" />
+    </>
+  );
+};
 
 function CourseMapModal() {
-  //const [showModal, setShowModal] = useState(false);
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [classRoom, setClassRoom] = useState([]);
+
+  const defaultProps = {
+    center: {
+      lat: lat,
+      lng: lng,
+    },
+    zoom: 19,
+  };
+
+  if (navigator.geolocation) {
+    // 執行要權限的function
+    function error() {
+      alert('無法取得你的位置');
+    }
+
+    // 使用者允許抓目前位置，回傳經緯度
+    function success(position) {
+      console.log(position.coords.latitude, position.coords.longitude);
+      setLat(position.coords.latitude);
+      setLng(position.coords.longitude);
+    }
+
+    // 跟使用者拿所在位置的權限
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    alert('Sorry, 你的裝置不支援地理位置功能。');
+  }
 
   return (
     <>
@@ -45,7 +82,15 @@ function CourseMapModal() {
               <button className="modal__store-btn">選擇店鋪</button>
             </div>
           </div>
-          <div className="modal__map"></div>
+          <div className="modal__map">
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: '' }}
+              defaultCenter={defaultProps.center}
+              defaultZoom={defaultProps.zoom}
+            >
+              <MarkerIcon lat={lat} lng={lng} />
+            </GoogleMapReact>
+          </div>
         </div>
         <button className="check__place-btn">確認店鋪</button>
       </div>
